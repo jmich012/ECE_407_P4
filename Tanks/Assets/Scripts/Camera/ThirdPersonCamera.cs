66f8135be88cf4ba5667179ea9e6427c76a4d05c
jmichal2024 <jamichalick2002@gmail.com>
2024-04-02 15:12:24 -0400

    updated 3rd person camera script

--- Tanks/Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Tanks/Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -2,25 +2,31 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public Transform tank; // The target object to follow
-    public Vector3 offset = new Vector3(0f,7f, -15f); // The offset from the target position
-    public float damping = 5f; // The smoothness of the camera movement
+    public Transform tankCamera;
+    public Transform mapCamera;
+    public float positionDamping = 0.02f;
+    public float rotationDamping = 0.01f;
 
-    void LateUpdate()
+
+    private bool playerCamera = true;
+
+    private void Update()
     {
-        if (tank != null)
+        if (Input.GetKeyDown(KeyCode.F)) {
+            playerCamera = !playerCamera;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (tankCamera != null && playerCamera)
         {
-            // Calculate the desired camera position based on the tank's position and offset
-            Vector3 desiredPosition = tank.position + offset;
-
-            // Smoothly move the camera towards the desired position
-            //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, damping * Time.deltaTime);
-            //transform.position = smoothedPosition;
-
-            transform.position = Vector3.Lerp(transform.position, desiredPosition, damping * Time.deltaTime);
-
-            // Make the camera look at the tank
-            transform.LookAt(tank);
+            transform.position = Vector3.Lerp(transform.position, tankCamera.position, positionDamping);
+            transform.rotation = Quaternion.Lerp(transform.rotation, tankCamera.rotation, rotationDamping);
+        }
+        else if (mapCamera != null && !playerCamera) 
+        {
+            transform.position = mapCamera.position;
+            transform.rotation = mapCamera.rotation;
         }
     }
 }
